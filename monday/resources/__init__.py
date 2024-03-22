83c612306877d95c4e7512fb695974a979745a6a
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-03-22 21:18:39 +0000

    added version to init

--- monday/resources/__init__.py
+++ monday/resources/__init__.py
@@ -1,5 +1,6 @@
 """This is the init file for the resources package."""
 
 from monday.resources.users import UserResource
+from monday.resources.versions import VersionResource
 
-__all__ = ["UserResource"]
+__all__ = ["UserResource", "VersionResource"]
