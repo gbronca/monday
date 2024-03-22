6f1e6f417c98f0f3c6eadb86ea89ffdbf7e21200
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-03-22 21:19:15 +0000

    Define Monday client

---
+++ monday/client.py
@@ -0,0 +1,25 @@
+"""Client for the Monday.com API."""
+
+from .__version__ import __version__
+from .resources import (
+    UserResource,
+    VersionResource,
+)
+
+
+class MondayClient:
+    """Client for the Monday.com API."""
+
+    def __init__(  # noqa: D107
+        self: "MondayClient",
+        api_key: str,
+        api_version: str | None,
+    ) -> None:
+        self.users = UserResource(api_key=api_key, api_version=api_version)
+        self.workspaces = VersionResource(api_key=api_key, api_version=api_version)
+
+    def __repr__(self: "MondayClient") -> str:  # noqa: D105
+        return f"MondayClient {__version__}"
+
+    def __str__(self: "MondayClient") -> str:  # noqa: D105
+        return f"MondayClient {__version__}"
