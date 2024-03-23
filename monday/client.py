a0d93f844e20528bd3cc372c2b72d6fd575038aa
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-03-23 10:55:54 +0000

    fix versions name and set default value for api_version

--- monday/client.py
+++ monday/client.py
@@ -13,10 +13,10 @@ class MondayClient:
     def __init__(  # noqa: D107
         self: "MondayClient",
         api_key: str,
-        api_version: str | None,
+        api_version: str | None = None,
     ) -> None:
         self.users = UserResource(api_key=api_key, api_version=api_version)
-        self.workspaces = VersionResource(api_key=api_key, api_version=api_version)
+        self.versions = VersionResource(api_key=api_key, api_version=api_version)
 
     def __repr__(self: "MondayClient") -> str:  # noqa: D105
         return f"MondayClient {__version__}"
