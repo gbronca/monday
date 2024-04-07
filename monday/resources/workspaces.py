f22dcc29cac358239e7e240e193ca0b9d3b44c8f
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-04-07 11:58:22 +0100

    Added users and teams mutations to workspace

--- monday/resources/workspaces.py
+++ monday/resources/workspaces.py
@@ -141,10 +141,134 @@ class WorkspaceResource(BaseResource):
         Returns:
             dict: dict response from the monday.com GraphQL API
         """
-        query = """mutation {
-            delete_workspace ( workspace_id: "%(id)s") {
+        query = f"""mutation {{
+            delete_workspace ( workspace_id: "{workspace_id}") {{
                 id
-            }
-        }""" % {"id": workspace_id}  # noqa: UP031
+            }}
+        }}"""
+
+        return self.client.execute(query)
+
+    def add_users_to_workspace(
+        self: "WorkspaceResource",
+        workspace_id: str,
+        user_ids: list[str] | str,
+        kind: Literal["subscriber", "owner"] = "subscriber",
+    ) -> dict:
+        """Allows you to add users to a workspace via the API.
+
+        You can specify which users to add to the workspace and their subscription type.
+
+        Args:
+            workspace_id (str): The workspace's unique identifier.
+            user_ids (str | [str]): The ID's of the users to add to the workspace.
+            kind (str): Kind of subscribers added: subscriber or owner.
+
+        Returns:
+            dict: dict response from the monday.com GraphQL API
+        """
+        if not isinstance(user_ids, list):
+            user_ids = [str(user_ids)]
+
+        query = f"""mutation
+        {{
+            add_users_to_workspace (
+                workspace_id: "{workspace_id}",
+                user_ids: {json.dumps(user_ids)},
+                kind: {kind}
+            ) {{
+                id
+            }}
+        }}"""
+
+        return self.client.execute(query)
+
+    def delete_users_from_workspace(
+        self: "WorkspaceResource",
+        workspace_id: str,
+        user_ids: list[str] | str,
+    ) -> dict:
+        """Allows you to delete users from a workspace via the API.
+
+        Args:
+            workspace_id (str): The workspace's unique identifier.
+            user_ids (str | [str]): The IDs of the users to remove from the workspace.
+
+        Returns:
+            dict: dict response from the monday.com GraphQL API
+        """
+        if not isinstance(user_ids, list):
+            user_ids = [str(user_ids)]
+
+        query = f"""mutation
+        {{
+            delete_users_from_workspace (
+                workspace_id: "{workspace_id}",
+                user_ids: {json.dumps(user_ids)}
+            ) {{
+                id
+            }}
+        }}"""
+
+        return self.client.execute(query)
+
+    def add_teams_to_workspace(
+        self: "WorkspaceResource",
+        workspace_id: str,
+        team_ids: list[str] | str,
+        kind: Literal["subscriber", "owner"] = "subscriber",
+    ) -> dict:
+        """Allows you to add teams to a workspace via the API.
+
+        Args:
+            workspace_id (str): The workspace's unique identifier.
+            team_ids (str | [str]): The ID's of the teams to add to the workspace.
+            kind (str): Kind of subscribers added: subscriber or owner.
+
+        Returns:
+            dict: dict response from the monday.com GraphQL API
+        """
+        if not isinstance(team_ids, list):
+            team_ids = [str(team_ids)]
+
+        query = f"""mutation
+        {{
+            add_teams_to_workspace (
+                workspace_id: "{workspace_id}",
+                team_ids: {json.dumps(team_ids)},
+                kind: {kind}
+            ) {{
+                id
+            }}
+        }}"""
+
+        return self.client.execute(query)
+
+    def delete_teams_from_workspace(
+        self: "WorkspaceResource",
+        workspace_id: str,
+        team_ids: list[str] | str,
+    ) -> dict:
+        """Allows you to delete teams from a workspace via the API.
+
+        Args:
+            workspace_id (str): The workspace's unique identifier.
+            team_ids (str | [str]): The ID's of the teams to remove from the workspace.
+
+        Returns:
+            dict: dict response from the monday.com GraphQL API
+        """
+        if not isinstance(team_ids, list):
+            team_ids = [str(team_ids)]
+
+        query = f"""mutation
+        {{
+            delete_teams_from_workspace (
+                workspace_id: "{workspace_id}",
+                team_ids: {json.dumps(team_ids)}
+            ) {{
+                id
+            }}
+        }}"""
 
         return self.client.execute(query)
