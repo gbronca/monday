14d814533c7e55bfad07bd73b2ec5a20ef31e974
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-08-12 19:40:54 +0100

    add teams to board

--- monday/resources/teams.py
+++ monday/resources/teams.py
@@ -1,5 +1,7 @@
 """This module provides the Team class for accessing the Teams endpoint."""
 
+from typing import Literal
+
 from monday.resources.base import BaseResource
 from monday.utils import parse_parameters
 
@@ -35,6 +37,34 @@ class TeamResource(BaseResource):
                     kind
                     name
                 }}
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def add_teams_to_board(
+        self: "TeamResource",
+        board_id: str,
+        team_ids: str | list[str],
+        kind: Literal["subscriber", "owner"] | None = None,
+    ) -> dict:
+        """Allows you to add teams to a board via the API.
+
+        Args:
+            board_id (str): The board's unique identifier.
+            team_ids (str | list(str)): The unique identifiers of the teams to add to
+                the board. You can pass -1 to subscribe everyone in an account to
+                the board
+            kind (str, option): The team's role: subscriber or owner. If the argument
+                is not used, the team will be added as a subscriber.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals())
+        query = f"""mutation {{
+            add_teams_to_board ({", ".join(parameters)}) {{
+                id
             }}
         }}"""
 
