d38d4b0a33a9f929e2b196ef80c2f2c8e8a086bb
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-08-13 21:27:32 +0100

    added missing teams mutations

--- monday/resources/teams.py
+++ monday/resources/teams.py
@@ -61,10 +61,150 @@ class TeamResource(BaseResource):
         Returns:
             (dict): Dict response from the monday.com GraphQL API.
         """
-        parameters = parse_parameters(locals())
+        parameters = parse_parameters(locals(), exclude=["kind"])
         query = f"""mutation {{
             add_teams_to_board ({", ".join(parameters)}) {{
                 id
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def add_users_to_team(
+        self: "TeamResource",
+        team_id: str,
+        user_ids: str | list[str],
+    ) -> dict:
+        """Allows you to add users to a team via the API.
+
+        Args:
+            team_id (str): The unique identifier of the team to add users to.
+            user_ids (str | list[str]): The unique identifiers of the users
+                to add to the team.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals())
+        query = f"""mutation {{
+            add_users_to_team ({", ".join(parameters)}) {{
+                successful_users {{
+                    name
+                    email
+                }}
+                failed_users {{
+                    name
+                    email
+                }}
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def add_teams_to_workspace(
+        self: "TeamResource",
+        team_ids: str | list[str],
+        workspace_id: str,
+        kind: Literal["subscriber", "owner"] | None = None,
+    ) -> dict:
+        """Allows you to add teams to a workspace via the API.
+
+        Args:
+            team_ids (str | list[str]): The unique identifiers of the teams to add
+                to the workspace.
+            workspace_id (str): The workspace's unique identifier.
+            kind (str): The team's role: owner or subscriber.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals(), exclude=["kind"])
+        query = f"""mutation {{
+            add_teams_to_workspace ({", ".join(parameters)}) {{
+                id
+                name
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def delete_teams_from_board(
+        self: "TeamResource",
+        board_id: str,
+        team_ids: str | list[str],
+    ) -> dict:
+        """Allows you to add teams to a workspace via the API.
+
+        Args:
+            board_id (str): The board's unique identifier.
+            team_ids (str | list[str]): The unique identifiers of the teams to delete
+                from the board.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals())
+        query = f"""mutation {{
+            delete_teams_from_board ({", ".join(parameters)}) {{
+                id
+                name
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def remove_users_from_team(
+        self: "TeamResource",
+        team_id: str,
+        user_ids: str | list[str],
+    ) -> dict:
+        """Allows you to remove users from a team via the API.
+
+        Args:
+            team_id (str): The unique identifier of the team to remove users from.
+            user_ids (str | list[str]): The unique identifiers of the users to remove
+                from the team.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals())
+        query = f"""mutation {{
+            remove_users_from_team ({", ".join(parameters)}) {{
+                successful_users {{
+                    name
+                    email
+                }}
+                failed_users {{
+                    name
+                    email
+                }}
+            }}
+        }}"""
+
+        return await self.client.execute(query)
+
+    async def delete_teams_from_workspace(
+        self: "TeamResource",
+        team_ids: str | list[str],
+        workspace_id: str,
+    ) -> dict:
+        """Allows you to delete teams from a workspace via the API.
+
+        Args:
+            team_ids (str | list[str]): The unique identifiers of the teams to delete
+                from the workspace.
+
+            workspace_id (str): The workspace's unique identifier.
+
+        Returns:
+            (dict): Dict response from the monday.com GraphQL API.
+        """
+        parameters = parse_parameters(locals())
+        query = f"""mutation {{
+            delete_teams_from_workspace ({", ".join(parameters)}) {{
+                id
+                name
             }}
         }}"""
 
