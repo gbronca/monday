2b3d416ba2f367df09de3f5b77af0f3e08b92589
Giovanni <42703789+gbronca@users.noreply.github.com>
2024-08-31 13:00:15 +0100

    types

---
+++ monday/resources/types/types.py
@@ -0,0 +1,47 @@
+from typing import Literal
+
+COLUMNTYPE = Literal[
+    "auto_number",
+    "board_relation",
+    "button",
+    "checkbox",
+    "color_picker",
+    "country",
+    "creation_log",
+    "date",
+    "dependency",
+    "doc",
+    "dropdown",
+    "email",
+    "file",
+    "formula",
+    "hour",
+    "item_assignees",
+    "item_id",
+    "last_updated",
+    "link",
+    "location",
+    "long_text",
+    "mirror",
+    "name",
+    "numbers",
+    "people",
+    "phone",
+    "progress",
+    "rating",
+    "status",
+    "subtasks",
+    "tags",
+    "team",
+    "text",
+    "timeline",
+    "time_tracking",
+    "vote",
+    "week",
+    "world_clock",
+    "unsupported",
+]
+
+OrderBy = Literal["created_at", "used_at"]
+
+STATE = Literal["active", "all", "archived", "deleted"]
